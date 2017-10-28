663efb3de1f6eb08b374f30a18413561634ffbf5
Mark Sturm <privat@mark-sturm.de>
2017-10-28 08:38:49 +0200

    push-notifications

--- android/app/src/main/java/com/epaper/MainApplication.java
+++ android/app/src/main/java/com/epaper/MainApplication.java
@@ -3,6 +3,7 @@ package com.epaper;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import com.pspdfkit.react.PSPDFKitPackage;
 import fm.indiecast.rnaudiostreamer.RNAudioStreamerPackage;
 import com.inprogress.reactnativeyoutube.ReactNativeYouTube;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeOneSignalPackage(),
             new PSPDFKitPackage(),
             new RNAudioStreamerPackage(),
             new ReactNativeYouTube(),
--- js/components/menu/DrawerContainer.js
+++ js/components/menu/DrawerContainer.js
@@ -8,7 +8,44 @@ import { NavigationActions } from 'react-navigation'
 
 import AwseomeIcon from 'react-native-vector-icons/FontAwesome';
 
+import OneSignal from 'react-native-onesignal'; // Import package from node modules
+
+
 class DrawerContainer extends React.Component {
+
+  componentWillMount() {
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('registered', this.onRegistered);
+    OneSignal.addEventListener('ids', this.onIds);
+}
+
+componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('registered', this.onRegistered);
+    OneSignal.removeEventListener('ids', this.onIds);
+}
+
+onReceived(notification) {
+    console.log("Notification received: ", notification);
+}
+
+onOpened(openResult) {
+  console.log('Message: ', openResult.notification.payload.body);
+  console.log('Data: ', openResult.notification.payload.additionalData);
+  console.log('isActive: ', openResult.notification.isAppInFocus);
+  console.log('openResult: ', openResult);
+}
+
+onRegistered(notifData) {
+    console.log("Device had been registered for push notifications!", notifData);
+}
+
+onIds(device) {
+console.log('Device info: ', device);
+}
+
   isActiveClass = (key)=>{
 
     if(this.props.activeItemKey.toLowerCase()===key.toLowerCase()){
